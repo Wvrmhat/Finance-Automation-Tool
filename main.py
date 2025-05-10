c985ecbea5881463e42ba8e866e1878e09302ce9
Wvrmhat <86238625+Wvrmhat@users.noreply.github.com>
2025-05-10 21:27:43 +0200

    Implemented feature to save changes
    Changes made in categories are also saved. Refreshing the page does not remove them.

--- main.py
+++ main.py
@@ -75,6 +75,8 @@ def main():
             debits_df = df[df["Debit/Credit"] == "Debit"].copy() 
             credits_df = df[df["Debit/Credit"] == "Credit"].copy() 
             
+            st.session_state.debits_df = debits_df.copy() 
+            
             tab1, tab2 = st.tabs(["Expenses (Debits)", "Payments (Credits)"])
             with tab1:
                 new_category = st.text_input("New Category Name")
@@ -86,7 +88,32 @@ def main():
                         save_categories()
                         st.rerun()
                         
-                st.write(debits_df)
+                st.subheader("Your Expenses")
+                edited_df = st.data_editor(
+                    st.session_state.debits_df[["Date", "Details", "Amount", "Category"]],
+                    column_config={
+                        "Date": st.column_config.DateColumn("Date", format="DD/MM/YYYY"),
+                        "Amount": st.column_config.NumberColumn("Amount", format="%.2f AED"),
+                        "Category": st.column_config.SelectboxColumn(
+                            "Category",
+                            options=list(st.session_state.categories.keys()) 
+                        )
+                    },
+                    hide_index=True,
+                    use_container_width=True, 
+                    key="category_editor"
+                )
+                save_button = st.button("Apply Changes", type="primary")
+                if save_button:
+                    for idx, row in edited_df.iterrows():
+                        new_category = row["Category"]
+                        
+                        # if a change was made to the category, we take the details and update it to modify the state and save it 
+                        if new_category == st.session_state.debits_df.at[idx, "Category"]:
+                            continue 
+                        details = row["Details"]
+                        st.session_state.debits_df.at[idx, "Category"] = new_category
+                        add_keyword_to_category(new_category, details) 
                 
             with tab2:
                 st.write(credits_df)
